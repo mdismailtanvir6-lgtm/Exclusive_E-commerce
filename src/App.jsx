80efbde0efdbcf369eadfbb490f0ec82821bf13b
Ismail Hossain <mdismailtanvir6-lgtm>
2025-10-01 15:48:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <p className="text-left text-5xl text-style bg-secondary_color font-inter">Hello React !</p>
+        <p className="text-left text-5xl text-style font-poppins">Hello React !</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
